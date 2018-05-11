bdac86643f514fab4c61519573563db7b0a6eff1
emanuellarini <emanuellarini@hotmail.com>
2018-05-10 22:25:12 -0300

    FIx splash and add wizard message

--- src/screens/guest/Wizard.js
+++ src/screens/guest/Wizard.js
@@ -23,9 +23,9 @@ class Wizard extends React.Component {
         </View>
         <View style={{alignItems: 'center', padding: 24, flex: 1, flexDirection: 'column', backgroundColor: '#fafafa', width: '100%' }}>
           <Title type="header" text="SEJA BEM-VINDO(A)" />
-          <Text style={{ fontSize: 18, marginVertical: 20, textAlign: 'center', color: '#95989A' }}>
-		        Somos uma empresa com a intenção de levar informações sobre leis, direitos, deveres e dia-a-dia de forma mais humana e acessível a sociedade.
-	        </Text>
+          <Text style={{ fontSize: 18, marginVertical: 40, textAlign: 'center', color: '#95989A' }}>
+            Aqui, neste aplicativo, você pode consultar a Nova CLT, assistir Vídeos e Animações sobre Leis, Direitos e Deveres que regulam nosso dia a dia.
+          </Text>
         </View>
         <Button block large onPress={this.handlePress}>
           <Text uppercase>próximo</Text>

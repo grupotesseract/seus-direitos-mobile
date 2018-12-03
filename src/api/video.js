bee3a301f889413a588ccb84a4ec205367e2ad75
Hugo Cicarelli <hcicarelli@gmail.com>
2018-12-03 19:11:29 -0200

    Fix API Videos

--- src/api/video.js
+++ src/api/video.js
@@ -2,7 +2,7 @@ import axios from "../utils/axios";
 import mockedVideos from './mocks/videos'
 
 export const getVideos = (data) => {
-  return axios().get('home', { params: data })
+  return axios().get('videos', { params: data })
     .then(res => res.data)
     .catch(err => err.response)
 }

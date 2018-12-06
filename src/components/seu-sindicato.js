aff59c4bfc3371be34bddc6e888d7ec06ee290a8
Hugo Cicarelli <hcicarelli@gmail.com>
2018-12-06 19:58:26 -0200

    Seu Sindicato - Tela de login

--- src/components/seu-sindicato.js
+++ src/components/seu-sindicato.js
@@ -3,24 +3,25 @@ import { StyleSheet, View, Image } from 'react-native'
 import {Text} from 'native-base'
 
 const styles = StyleSheet.create({
-  img: {
-    width: '100%',
-    resizeMode: Image.resizeMode.contain,
-    marginBottom: 16
-  },
-  margin: {
-    marginVertical: 16
-  },
-  container: {
-    alignItems: 'center',
-    flex: 1,
-  }
+    img: {
+        width: '100%',
+        resizeMode: Image.resizeMode.contain,
+        marginBottom: 16
+    },
+    margin: {
+        marginVertical: 16
+    },
+    container: {
+        alignItems: 'center',
+        display: 'flex',
+        width: '100%'
+    }
 })
 
 export default function ({ noMargin = false }) {
-  return (
-    <View style={[styles.container, !noMargin && styles.margin]}>
-      <Image source={require('../../assets/icons/seu-sindicato.png')} style={styles.img} />
-    </View>
-  )
+    return (
+        <View style={[styles.container, !noMargin && styles.margin]}>
+            <Image source={require('../../assets/icons/seu-sindicato.png')} style={styles.img} />
+        </View>
+    )
 }
